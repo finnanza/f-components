de403afdea99d051f4a352ae83530e999a36bea7
Marvin Parada <hello@marvin.dev>
2024-06-30 19:04:16 -0700

    Remove default files.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,12 @@
-import HomeView from '../views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
       name: 'home',
       path: '/',
-    },
-    {
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      path: '/about',
     },
   ],
 })
